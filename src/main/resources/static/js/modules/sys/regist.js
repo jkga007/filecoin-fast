fa4288701c48d9bfabcf88334d96d579fff1f4ff
jkgao007 <jkgao007@163.com>
2018-06-25 20:40:15 +0800

    编写重新发送邮件整体逻辑

--- src/main/resources/static/js/modules/sys/regist.js
+++ src/main/resources/static/js/modules/sys/regist.js
@@ -44,7 +44,62 @@ var RegistFunc = (function () {
             }
         }, true,true);
 
+    };
 
+    /**
+     * 返回修改基本信息
+     */
+    registFunc.returnEditMail = function () {
+
+    };
+
+    /**
+     * 重新发送邮件信息
+     */
+    registFunc.resendMail = function (obj) {
+        var _this = $(obj);
+        var times = 1 * 60;
+        _this.html(times + '秒后可以重新发送');
+        var timer = setInterval(function(){
+            times--;
+            _this.html(times + '秒后可以重新发送');
+            if(times == 0){
+                _this.html('重新发送');
+                _this.attr('onclick','RegistFunc.resendMail(this);');
+                _this.attr('style','');
+                clearInterval(timer);
+            }
+        },1000);
+        _this.attr('onclick','javascript:return false;');
+        _this.attr('style','opacity: 0.2');
+        /**
+         * 发送邮件
+         */
+        var userMail = $.trim($("#user_mail").val());
+        var userId = $.trim($("#user_id").val());
+        if(userMail.length != 0 && userId.length != 0){
+            var path = ctx + "/sys/resendMail";
+            var ajax = new AJAXPacket(path, "正在重新发送邮件...请稍后");
+
+            ajax.add("userMail", userMail);
+            ajax.add("userId", userId);
+
+            Core.sendPacket(ajax, function (packet) {
+                var resultCode = packet.code;
+                var message = packet.msg;
+                if (resultCode == "0") {
+                    Core.alert("重新发送邮件成功!,请注意查收!", 1,false, function () {
+                    });
+                } else {
+                    Core.alert(message, 2,false, function () {
+                    });
+                }
+            }, true,true);
+        }else{
+            Core.alert("您的操作有误,请重新注册!", 2,false, function () {
+
+            });
+        }
     };
 
 
