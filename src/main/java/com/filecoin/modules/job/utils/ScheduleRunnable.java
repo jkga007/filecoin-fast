a118e2f6129586e76e8cd7a65b5fbefbb8e8c742
jkgao007 <jkgao007@163.com>
2018-07-06 21:18:23 +0800

    无

--- src/main/java/com/filecoin/modules/job/utils/ScheduleRunnable.java
+++ src/main/java/com/filecoin/modules/job/utils/ScheduleRunnable.java
@@ -10,40 +10,40 @@ import java.lang.reflect.Method;
 
 /**
  * 执行定时任务
- * 
- * @author r25437,g20416
+ *
+ * @author r25437, g20416
  * @email support@filecoinon.com
  * @date 2016年11月30日 下午12:49:33
  */
 public class ScheduleRunnable implements Runnable {
-	private Object target;
-	private Method method;
-	private String params;
-	
-	public ScheduleRunnable(String beanName, String methodName, String params) throws NoSuchMethodException, SecurityException {
-		target = SpringContextUtils.getBean(beanName);
-		this.params = params;
-		
-		if(StringUtils.isNotBlank(params)){
-			method = target.getClass().getDeclaredMethod(methodName, String.class);
-		}else{
-			method = target.getClass().getDeclaredMethod(methodName);
-		}
-	}
+    private Object target;
+    private Method method;
+    private String params;
 
-	@Override
-	public void run() {
-		try {
-			System.out.println("查看当前线程名称-------------" + Thread.currentThread().getName() + "--ID==" + Thread.currentThread().getId());
-			ReflectionUtils.makeAccessible(method);
-			if(StringUtils.isNotBlank(params)){
-				method.invoke(target, params);
-			}else{
-				method.invoke(target);
-			}
-		}catch (Exception e) {
-			throw new FileCoinException("执行定时任务失败", e);
-		}
-	}
+    public ScheduleRunnable(String beanName, String methodName, String params) throws NoSuchMethodException, SecurityException {
+        target = SpringContextUtils.getBean(beanName);
+        this.params = params;
+
+        if (StringUtils.isNotBlank(params)) {
+            method = target.getClass().getDeclaredMethod(methodName, String.class);
+        } else {
+            method = target.getClass().getDeclaredMethod(methodName);
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+//			System.out.println("查看当前线程名称-------------" + Thread.currentThread().getName() + "--ID==" + Thread.currentThread().getId());
+            ReflectionUtils.makeAccessible(method);
+            if (StringUtils.isNotBlank(params)) {
+                method.invoke(target, params);
+            } else {
+                method.invoke(target);
+            }
+        } catch (Exception e) {
+            throw new FileCoinException("执行定时任务失败", e);
+        }
+    }
 
 }
